c3ede3166ef4c9213e39e272cb230e7ac0244264
oronsultan <oronsultan@hotmail.com>
2019-02-17 18:29:47 +0200

    .

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  Recipes: Recipe [] = [
+    new Recipe('A Test Recipe', 'This is simply a tets',
+    'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d'),
+    new Recipe('A Test Recipe', 'This is simply a tets',
+    'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d')
+  ];
   constructor() { }
 
   ngOnInit() {
